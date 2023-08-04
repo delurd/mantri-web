0b46feb9af5366864e188caf123c013100503a8a
fadhlur <4hmad.fadhlur@gmail.com>
2023-08-04 20:57:18 +0700

    chore: update illustration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 import s from './page.module.css';
 import ButtonStatus from '@/components/ButtonStatus/ButtonStatus';
 import InfoBanner from '@/components/InfoBanner/InfoBanner';
-import { host } from '@/utils/variables';
+import {host} from '@/utils/variables';
 import ClientLog from '@/components/ClientLog/ClientLog';
 
 const getDataStatus = async () => {
   let dataReturn = {};
-  const res = await fetch(host+'/api/practice-status');
+  const res = await fetch(host + '/api/practice-status');
   const json = await res.json();
 
   const data = json.data;
@@ -18,29 +18,22 @@ export default async function Home() {
   const data = await getDataStatus();
 
   return (
-    <main
-      className={s.itemCenter}
-      style={{
-        height: '100vh',
-        backgroundColor: 'white',
-        backgroundImage: 'url("/bg-patern.png")',
-        backgroundRepeat: 'repeat',
-      }}
-    >
+    <main className={s.itemCenter + ' ' + s.main} style={{}}>
       <div className={s.card}>
         <div style={{display: 'flex'}}>
-          <div style={{flex: 1, marginTop: '25px'}}>
+          <div className={s.colTitle}>
             <h2>Praktik Mantri Mejeruk</h2>
           </div>
-          <div style={{flex: 1, position: 'relative'}}>
-            <Image
+          <div className={s.colImage}>
+            <div className={s.illustrationImage} />
+            {/* <Image
               src="/illustration-doctor.PNG"
               alt="illustration"
               style={{position: 'absolute', bottom: 0, right: 0}}
               width={150}
               height={170}
               priority
-            />
+            /> */}
           </div>
         </div>
         <div className={s.itemCenter} style={{flexDirection: 'column'}}>
