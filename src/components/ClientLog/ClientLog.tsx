2d6202870842ddefae3aedc80ff7191aa287d299
fadhlur <4hmad.fadhlur@gmail.com>
2023-08-04 19:41:03 +0700

    chore: update log time

--- src/components/ClientLog/ClientLog.tsx
+++ src/components/ClientLog/ClientLog.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import {host} from '@/utils/variables';
 import moment from 'moment';
 import React, {useEffect} from 'react';
 
@@ -7,11 +8,19 @@ type Props = {};
 
 const ClientLog = (props: Props) => {
   useEffect(() => {
-    moment().utcOffset(0)
-    
+    getDataStatus();
     console.log(moment().utcOffset(0).format());
     console.log(moment().format() + '--' + moment.locale());
   }, []);
+
+  const getDataStatus = async () => {
+    const res = await fetch(host + '/api/practice-status');
+    const json = await res.json();
+
+    const data = json.data;
+
+    console.log(data);
+  };
   return <></>;
 };
 
