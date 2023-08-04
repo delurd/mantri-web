5b025a32055e3070384696609854dee6f73b8411
fadhlur <4hmad.fadhlur@gmail.com>
2023-08-04 17:19:47 +0700

    feat: update component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,52 @@
 import Image from 'next/image';
 import s from './page.module.css';
-import FooterCard from '@/components/FooterCard/FooterCard';
+import ButtonStatus from '@/components/ButtonStatus/ButtonStatus';
+import InfoBanner from '@/components/InfoBanner/InfoBanner';
+import { host } from '@/utils/variables';
 
-export default function Home() {
+const getDataStatus = async () => {
+  let dataReturn = {};
+  const res = await fetch(host+'/api/practice-status');
+  const json = await res.json();
+
+  const data = json.data;
+  return data;
+};
+
+export default async function Home() {
+  const data = await getDataStatus();
+
   return (
-    <main>
-      <div
-        className={s.itemCenter}
-        style={{
-          height: '100vh',
-          backgroundColor: 'white',
-          backgroundImage: 'url("/bg-patern.png")',
-          backgroundRepeat: 'repeat',
-        }}
-      >
-        <div className={s.card}>
-          <div style={{display: 'flex'}}>
-            <div style={{flex: 1, marginTop: '25px'}}>
-              <h2>Praktik Mantri Mejeruk</h2>
-            </div>
-            <div style={{flex: 1, position: 'relative'}}>
-              <img
-                src="/illustration-doctor.png"
-                alt="illustration"
-                style={{position: 'absolute', bottom: 0, right: 0}}
-                width={150}
-                height={170}
-              />
-            </div>
+    <div
+      className={s.itemCenter}
+      style={{
+        height: '100vh',
+        backgroundColor: 'white',
+        backgroundImage: 'url("/bg-patern.png")',
+        backgroundRepeat: 'repeat',
+      }}
+    >
+      <div className={s.card}>
+        <div style={{display: 'flex'}}>
+          <div style={{flex: 1, marginTop: '25px'}}>
+            <h2>Praktik Mantri Mejeruk</h2>
           </div>
-          <FooterCard />
+          <div style={{flex: 1, position: 'relative'}}>
+            <Image
+              src="/illustration-doctor.png"
+              alt="illustration"
+              style={{position: 'absolute', bottom: 0, right: 0}}
+              width={150}
+              height={170}
+              priority
+            />
+          </div>
+        </div>
+        <div className={s.itemCenter} style={{flexDirection: 'column'}}>
+          <ButtonStatus status={data?.status} />
+          <InfoBanner data={data} />
         </div>
       </div>
-    </main>
+    </div>
   );
 }
---
+++ src/components/InfoBanner/InfoBanner.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+type Props = {
+  data?: any;
+};
+
+const InfoBanner = (props: Props) => {
+  return (
+    <>
+      {!props.data?.information.includes('sholat') ? (
+        <></>
+      ) : (
+        <div style={{marginTop: '10px'}}>
+          <p
+            style={{
+              // padding: '10px 20px',
+              // border: '1px solid #68B3DD',
+              borderRadius: '5px',
+              fontSize: '14px',
+            }}
+          >
+            {props.data?.information}
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default InfoBanner;
