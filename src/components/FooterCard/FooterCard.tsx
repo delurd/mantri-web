940e895357b9cc268539588fad076746fd8fe5c4
fadhlur <4hmad.fadhlur@gmail.com>
2023-08-04 16:35:19 +0700

    feat: add env

--- src/components/FooterCard/FooterCard.tsx
+++ src/components/FooterCard/FooterCard.tsx
@@ -2,15 +2,16 @@
 
 import React, {useMemo, useState} from 'react';
 import s from './footerCard.module.css';
+import {host} from '@/utils/variables';
 
 type Props = {};
 
 const FooterCard = (props: Props) => {
   const [data, setData] = useState<any>();
 
-  const getDataStatus = useMemo(async () => {
+  const getDataStatus = useMemo(async () => {    
     let dataReturn = {status: 'open'};
-    const res = await fetch('https://mantri-web.vercel.app/api/practice-status');
+    const res = await fetch(host + '/api/practice-status');
     const json = await res.json();
 
     const data = json.data;
---
+++ src/utils/variables.ts
@@ -0,0 +1,3 @@
+const host = process.env.NEXT_PUBLIC_API_HOST || 'localhost'
+
+export { host }
