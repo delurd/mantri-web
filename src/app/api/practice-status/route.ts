2d796f051c997637a0b5e71210ec1b0eba84ddaf
fadhlur <4hmad.fadhlur@gmail.com>
2023-08-05 19:24:20 +0700

    feat: update post

--- src/app/api/practice-status/route.ts
+++ src/app/api/practice-status/route.ts
@@ -90,14 +90,82 @@ export const GET = async (request: NextRequest, response: Response) => {
 export const POST = async (request: NextRequest) => {
     const body = await request.json()
     console.log('post');
-
     console.log(body);
 
+    let statusPractice: 'close' | 'open' = 'close'
+    let informasiDetail = ''
+
+    const thisMonth: number = parseInt(moment(body.time).utcOffset(7).format("M"));
+    const thisDay: number = parseInt(moment(body.time).utcOffset(7).format("D"));
+    const thisTime = moment(body.time).utcOffset(7).format('HH:mm')
+
+    const jadwalSholatBulanan = Object.values(jadwalSholatLamongan)[thisMonth - 1]
+    const jadwalSholatHarian = Object.values(jadwalSholatBulanan)[thisDay - 1]
+
+    const jumlahWaktuSholat = Object.keys(jadwalSholatHarian).length;
+    const waktuIstirahat = 20;
+
+    //CHECK JAM BUKA
+    for (let x = 0; x < jamPraktikBuka.length; x++) {
+        const jamBuka = jamPraktikBuka[x]
+
+        // console.log('jam buka ' + jamBuka.start);
+
+        //JIKA JAM SESUAI JAM BUKA MAKA OPEN
+        if (thisTime >= jamBuka.start &&
+            thisTime <= jamBuka.end
+        ) {
+            statusPractice = "open";
+            informasiDetail = info.buka
+
+            //CHECK WAKTU ADZAN
+            for (let i = 0; i < jumlahWaktuSholat; i++) {
+                const waktuAdzan = Object.values(jadwalSholatHarian)[i]
+                let waktuAdzanHour = parseInt(waktuAdzan.split(':')[0])
+                let waktuAdzanMinute = parseInt(waktuAdzan.split(':')[1])
+
+                //CALCULATE WAKTU ISTIRAHAT SHOLAT
+                let waktuIstirahatSholatHour = waktuAdzanHour
+                let waktuIstirahatSholatMinutes = waktuAdzanMinute + waktuIstirahat
+
+                if (waktuIstirahatSholatMinutes > 59) {
+                    waktuIstirahatSholatMinutes -= 60
+                    waktuIstirahatSholatHour += 1
+                }
+                const waktuIstirahatSholat = moment((waktuIstirahatSholatHour + ':' + waktuIstirahatSholatMinutes), 'h:m').format('HH:mm')
+
+                //CALCULATE PERSIAPAN ADZAN / SHOLAT
+                const miliPraAdzan = 10 * 60 //MENIT PER DETIK
+                const miliWaktuAdzan = parseInt(moment(waktuAdzan, 'HH:mm').format('X'))
+                const praWaktuAdzan = moment((miliWaktuAdzan - miliPraAdzan), 'X').format('HH:mm')
+
+                //JIKA TIBA WAKTU SHOLAT MAKA TUTUP
+                if (thisTime >= praWaktuAdzan &&
+                    thisTime <= waktuIstirahatSholat
+                ) {
+                    statusPractice = 'close'
+                    informasiDetail = info.sholat
+                    break;
+                } else {
+                    statusPractice = 'open'
+                }
+            }
+
+            break;
+        } else {
+            statusPractice = 'close'
+            informasiDetail = info.tutup
+        }
+    }
+
+    console.log("statusPractice " + statusPractice + moment.locale());
+
     const dataReturn = {
-        date : moment(body.time).utcOffset(7).format("DD-MM-YYYY"),
-        clock: moment(body.time).utcOffset(7).format("HH:mm"),
+        date: moment(body.time).utcOffset(7).format("DD-MM-YYYY"),
+        clock: thisTime,
+        waktuSholatHariIni: jadwalSholatHarian
     }
 
 
-    return NextResponse.json({ message: 'success' , data: dataReturn}, { status: 200 })
+    return NextResponse.json({ message: 'success', data: dataReturn }, { status: 200 })
 }
